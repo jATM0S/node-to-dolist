20f827e5a12f6fe3601722cbd8db557bab3827fb
_jATMOS <bks.mhryadayada@gmail.com>
2023-11-24 00:30:49 +0545

    removing comments

--- app.js
+++ app.js
@@ -5,16 +5,9 @@ const port = 3000;
 
 app.use(express.json());
 
-// controllers
 
 const router=require ('./routers');
 app.use("/todos",router);
-// const router =express.Router();
-// router.route("/todos")
-//   .get(getTodos)
-//   .post(postTodos)
-//   .patch(updateTodos)
-//   .delete(deleteTodos);
 
 app.listen(3000, () => {
   console.log(`Listening on port ${port}`);
