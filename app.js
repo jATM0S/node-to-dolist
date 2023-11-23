badb55722d5b00749c79dc6f35b9aa08962ac701
_jATMOS <bks.mhryadayada@gmail.com>
2023-11-23 12:23:22 +0545

    making the patch api to update the todo of a particular id

--- app.js
+++ app.js
@@ -82,6 +82,51 @@ app.delete("/", (req, res) => {
   }
 });
 
+app.patch("/", (req, res) => {
+  try {
+    const id = req.body.id;
+    // getting the taskIDs by creating a funciton that stores the ids in a array in task id
+    let taskIDs = [];
+    const getTaskIds = () => {
+      taskIDs = Object.keys(todos);
+    };
+
+    getTaskIds();
+    let taskExist = false;
+    for (let i = 1; i < taskIDs.length; i++) {
+      if (id == taskIDs[i]) {
+        taskExist = true;
+      }
+    }
+
+    //this checks if task exists then if it exists it changes the task
+    if (taskExist) {
+      // let idBody=todos[id];
+      let changeBody = req.body.changes;
+      console.log(changeBody);
+      if (todos[id].task !== changeBody.task) {
+        todos[id].task = changeBody.task;
+      }
+      if (todos[id].description !== changeBody.description) {
+        todos[id].description = changeBody.description;
+      }
+      if (todos[id].completed !== changeBody.completed) {
+        todos[id].completed = changeBody.completed;
+      }
+
+      //writing the modified todo after deleting in json file
+      const updateTodo = JSON.stringify(todos, null, 2);
+      fs.writeFileSync("./todo.json", updateTodo);
+      res.status(200).send(`sucessfully updated todo ${id}`);
+    } else {
+      res.status(400).send("error");
+    }
+  } catch {
+    res.status(400).send("error");
+  }
+});
+
+
 app.listen(3000, () => {
   console.log(`Listening on port ${port}`);
 });
