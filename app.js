e50871e6be74342bf907d0633a43eeeae3b12de4
_jATMOS <bks.mhryadayada@gmail.com>
2023-11-23 08:50:28 +0545

    shifting the id to the deleted todo and deleting last undefined todo

--- app.js
+++ app.js
@@ -20,14 +20,14 @@ app.post("/", (req, res) => {
   try {
     //taking the number of value of the todo
     const taskIDs = Object.keys(todos);
-    const newId = taskIDs.length+1;
-    
+    const newId = taskIDs.length + 1;
+
     //adding todo to the newId
-    todos[newId]=req.body;
+    todos[newId] = req.body;
 
     //stringfying the tobe added todo then writing the whole todo to json so that the todo added to "todos" is also added
-    const addTodo= JSON.stringify(todos, null, 2);
-    fs.writeFileSync('./todo.json',addTodo);
+    const addTodo = JSON.stringify(todos, null, 2);
+    fs.writeFileSync("./todo.json", addTodo);
     console.log(todos[newId]);
     res.status(200).send("success");
   } catch {
@@ -35,41 +35,52 @@ app.post("/", (req, res) => {
   }
 });
 
-app.delete("/",(req,res)=>{
-try{
-  const id=req.body.id;
-  //getting the taskIDs by creating a funciton that stores the ids in a array in task id 
-  let taskIDs=[];
-  const getTaskIds =()=>{
-    taskIDs=Object.keys(todos);
-  }
-  getTaskIds();
-  console.log(taskIDs);
+app.delete("/", (req, res) => {
+  try {
+    const id = req.body.id;
+    //getting the taskIDs by creating a funciton that stores the ids in a array in task id
+    let taskIDs = [];
+    const getTaskIds = () => {
+      taskIDs = Object.keys(todos);
+    };
 
-  let taskExist=false;
-  for (let i = 1; i < taskIDs.length; i++) {
-    if(id==taskIDs[i]){
-      taskExist=true;
+    getTaskIds();
+    console.log(taskIDs);
+    
+    let taskExist = false;
+    for (let i = 1; i < taskIDs.length; i++) {
+      if (id == taskIDs[i]) {
+        taskExist = true;
+      }
     }
-}
 
-//this checks if task exists then if it exists it deletes and prints the no of tasks present in the file
-if (taskExist) {
-  delete todos[id];
-  const deleteTodo= JSON.stringify(todos, null, 2);
-  fs.writeFileSync('./todo.json',deleteTodo);
-  getTaskIds();
-  console.log(todos);
-  console.log(taskIDs);
-  res.status(200).send(`Deleted todo no ${id}`);
-}else{
-res.status(400).send("number doesnt exist");
-}
+    //this checks if task exists then if it exists it deletes and prints the no of tasks present in the file
+    if (taskExist) {
+      delete todos[id];
+      //shifting the todo to the deleted todo and deleting last todo 
+      for (let i = id; i <= taskIDs.length; i++) {
+        let temp = todos[i+1];
+        todos[i]=temp;
+      }
+      delete todos[taskIDs.length];
+      //writing the modified todo after deleting in json file
+      const deleteTodo = JSON.stringify(todos, null, 2);
+      fs.writeFileSync("./todo.json", deleteTodo);
 
-}catch{
-  res.status(400).send("error");
-}
-})
+      //getting the updated todos
+      getTaskIds();
+      console.log(todos);
+      console.log(taskIDs);
+      //sending feedback 
+      res.status(200).send(`Deleted todo no ${id}`);
+    } else {
+      console.log(todos);
+      res.status(400).send("number doesnt exist");
+    }
+  } catch {
+    res.status(400).send("error");
+  }
+});
 
 app.listen(3000, () => {
   console.log(`Listening on port ${port}`);
